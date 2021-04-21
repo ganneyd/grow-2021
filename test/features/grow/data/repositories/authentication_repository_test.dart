447db563493c0686abc2a47d78cf8671c43e5f7f
GanneyADortch <ganneyd@gmail.com>
2021-04-20 21:43:38 -0400

    add mock auth repo and stub the appropriate methods

--- test/features/grow/data/repositories/authentication_repository_test.dart
+++ test/features/grow/data/repositories/authentication_repository_test.dart
@@ -1,63 +1,68 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grow_run_v1/core/error/failures.dart';
-import 'package:grow_run_v1/features/grow/data/repositories/authentication_repository.dart';
 import 'package:grow_run_v1/features/grow/domain/entities/user/user_entity.dart';
 import 'package:grow_run_v1/features/grow/domain/repositories/authentication_repository.dart';
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:dartz/dartz.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'authentication_repository_test.mocks.dart';
 
 @GenerateMocks(<Type>[AuthenticationRepository])
 void main() {
   const String email1 = 'ganneyd@gmail.com';
-  const String acceptablePassword = '123456';
-  final MockUser user = MockUser(
-    uid: 'userid',
-    email: email1,
-    displayName: 'Bob',
-  );
-  final MockFirebaseAuth mockFirebaseAuth = MockFirebaseAuth(mockUser: user);
-  final AuthenticationRepository authenticationRepository =
-      AuthenticationRepositoryImplementation(mockFirebaseAuth);
+  const String acceptablePassword = '1234456';
+  const UserEntity expectedUserEntity =
+      UserEntity(userEmail: 'expected@email.com', userID: 'expected-id');
+  final MockAuthenticationRepository authenticationRepository =
+      MockAuthenticationRepository();
 
-  group('Test that should not return with failures', () {
+  group('Test that should not return with failures, ', () {
     test(
       'should return a UserEntity() for the user when they sign up',
       () async {
         // arrange
-
+        when(authenticationRepository.signUpUser(any, any)).thenAnswer(
+            (_) async => const Right<Failure, UserEntity>(expectedUserEntity));
         // act
         final Either<Failure, UserEntity> result =
             await authenticationRepository.signUpUser('email', 'password');
+        final UserEntity actualUser = result.getOrElse(
+            () => const UserEntity(userID: 'userID', userEmail: 'userEmail'));
         // assert
-        expect(result, isInstanceOf<Right<Failure, UserEntity>>());
+        expect(actualUser, expectedUserEntity);
       },
     );
     test(
       'should return a UserEntity with the authenticated user',
       () async {
         // arrange
-
+        when(authenticationRepository.authenticateUser(any, any)).thenAnswer(
+            (_) async => const Right<Failure, UserEntity>(expectedUserEntity));
         // act
         final Either<Failure, UserEntity> result =
             await authenticationRepository.authenticateUser(
                 'email', 'password');
-
+        final UserEntity actualUser = result.getOrElse(
+            () => const UserEntity(userID: 'userID', userEmail: 'userEmail'));
         // assert
 
-        expect(result, '');
+        expect(actualUser, expectedUserEntity);
       },
     );
     test(
       'should return a Failure since firebased cannot be initialzed',
       () async {
         // arrange
-
+        when(authenticationRepository.registerUser(any, any)).thenAnswer(
+            (_) async => const Right<Failure, UserEntity>(expectedUserEntity));
         // act
         final Either<Failure, UserEntity> result =
             await authenticationRepository.registerUser('email', 'password');
+        final UserEntity actualUser = result.getOrElse(
+            () => const UserEntity(userID: 'userID', userEmail: 'userEmail'));
         // assert
-        expect(result, Left<Failure, UserEntity>(AuthenticationFailure()));
+        expect(actualUser, expectedUserEntity);
       },
     );
 
@@ -65,13 +70,16 @@ void main() {
       'should return a UserEntity() for the signed in user when they log in',
       () async {
         // arrange
-
+        when(authenticationRepository.loginUser(any, any)).thenAnswer(
+            (_) async => const Right<Failure, UserEntity>(expectedUserEntity));
         // ac
         final Either<Failure, UserEntity> result =
             await authenticationRepository.loginUser(
                 email1, acceptablePassword);
+        final UserEntity actualUser = result.getOrElse(
+            () => const UserEntity(userID: 'userID', userEmail: 'userEmail'));
         // assert
-        expect(result, isInstanceOf<Right<Failure, UserEntity>>());
+        expect(actualUser, expectedUserEntity);
       },
     );
 
@@ -79,7 +87,8 @@ void main() {
       'should return void when the user log outs',
       () async {
         // arrange
-
+        when(authenticationRepository.signOutUser())
+            .thenAnswer((_) async => Right<Failure, void>(() {}));
         // act
         final Either<Failure, void> result =
             await authenticationRepository.signOutUser();
