9955f1d91ab5666653cb92f0088847ed31973c5e
GanneyADortch <ganneyd@gmail.com>
2021-04-25 15:22:02 -0400

    add display name

--- lib/features/grow/domain/entities/user/user_entity.dart
+++ lib/features/grow/domain/entities/user/user_entity.dart
@@ -1,19 +1,29 @@
-import 'package:equatable/equatable.dart';
-
 ///Information that belongs to the currently signed in user,
 ///such as their [email] and [uid]
 class UserEntity {
   ///Constructor takes
   ///the [userid]
   ///and [userEmail]
+  ///and [name]
   const UserEntity({
     required this.userID,
     required this.userEmail,
+    required this.name,
   });
 
   ///The id for the user
   final String userID;
 
-  ///the type of  user;
+  ///The user's email
   final String userEmail;
+
+  ///The user's display name
+  final String name;
+
+  /// Empty user which represents an unauthenticated user.
+  static const UserEntity empty = UserEntity(
+    userEmail: '',
+    userID: '',
+    name: ' ',
+  );
 }
