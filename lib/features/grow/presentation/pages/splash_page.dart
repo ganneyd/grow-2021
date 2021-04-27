25d4c8c11cd9b6dabeed3abbf7264952b7c3e61b
GanneyADortch <ganneyd@gmail.com>
2021-04-26 22:06:47 -0400

    add splash screen page

--- lib/features/grow/presentation/pages/splash_page.dart
+++ lib/features/grow/presentation/pages/splash_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
+///Displays a circular progress indicator
 class SplashPage extends StatelessWidget {
-  static Route route() {
+  ///
+  static Route<dynamic> route() {
     return MaterialPageRoute<void>(builder: (_) => SplashPage());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(child: CircularProgressIndicator()),
     );
   }
