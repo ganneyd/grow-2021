661f073f8c98f5629eaeb8470f43a00aa5519c85
GanneyADortch <ganneyd@gmail.com>
2021-05-23 12:28:33 -0400

    page 2 of the child sign up form

--- lib/features/grow/presentation/child/widgets/form_widget/form_widget_2.dart
+++ lib/features/grow/presentation/child/widgets/form_widget/form_widget_2.dart
@@ -1,30 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:grow_run_v1/features/grow/data/models/school/school_model.dart';
 import 'package:grow_run_v1/features/grow/presentation/child/widgets/form_group/sign_up_form_group.dart';
+import 'package:grow_run_v1/features/grow/presentation/utils/forms_interface.dart';
+import 'package:grow_run_v1/features/grow/presentation/widgets/default_ui_elements.dart';
+import 'package:grow_run_v1/features/grow/presentation/widgets/form_group/get_form_validators.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 ///
-class ChildSignUpFormTwo extends StatelessWidget {
+class ChildSignUpFormTwo extends ReactiveFormGroup
+    implements GetFormValidation {
   ///
-  const ChildSignUpFormTwo({required this.schools})
+  ChildSignUpFormTwo({required this.schools})
       : super(key: const Key('child-sign-up-form-2'));
-  final List<DropdownMenuItem<SchoolModel>> schools;
-
+  final List<SchoolModel> schools;
+  final FormGroup _formGroup = ChildSignUpForm.buildChildSignUpPage2(
+      minAge: 7, maxAge: 17, min: 0, max: 12);
   @override
   Widget build(BuildContext context) {
-    return ReactiveForm(
-        formGroup: ChildSignUpForm.buildChildSignUpPage2(min: 0, max: 12),
-        child: Column(
-          children: <Widget>[
-            ReactiveTextField<int>(
-              formControlName: 'age',
-            ),
-            ReactiveDropdownField<SchoolModel>(
-                formControlName: 'school', items: schools),
-            ReactiveTextField<int>(
-              formControlName: 'grade_level',
-            ),
-          ],
-        ));
+    return DefaultUIElements.getDefaultPaddingContainer(
+        child: ReactiveFormBuilder(
+            form: () => _formGroup,
+            builder: (context, form, child) {
+              return Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ReactiveTextField<int>(
+                      formControlName: 'age',
+                      decoration: const InputDecoration(hintText: 'Age'),
+                    ),
+                  ),
+                  DefaultUIElements.getDefaultFormPadding(),
+                  Expanded(
+                      child: ReactiveDropdownField<SchoolModel>(
+                          formControlName: 'school',
+                          items: schools.map((SchoolModel e) {
+                            return DropdownMenuItem<SchoolModel>(
+                              value: e,
+                              child: Text(e.name),
+                            );
+                          }).toList())),
+                  DefaultUIElements.getDefaultFormPadding(),
+                  Expanded(
+                      child: ReactiveTextField<int>(
+                    formControlName: 'grade_level',
+                    decoration: const InputDecoration(hintText: 'Grade Level'),
+                  )),
+                ],
+              );
+            }));
+  }
+
+  @override
+  FormGroup get reactiveFormGroup => _formGroup;
+
+  @override
+  bool isValid() {
+    // TODO: implement isValid
+    throw UnimplementedError();
   }
 }
