c318d758812f4ca9b21b1cfcbd6d7dff7459f93d
GanneyADortch <ganneyd@gmail.com>
2021-05-23 12:16:23 -0400

    add text theme data

--- lib/features/grow/theme.dart
+++ lib/features/grow/theme.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 //TODO change to match GROW's app theme from old app
-final theme = ThemeData(
+final ThemeData theme = ThemeData(
+  primaryTextTheme: TextTheme(
+      headline2: const TextStyle(color: Colors.black54),
+      bodyText1: TextStyle(color: Colors.grey.shade700),
+      bodyText2: const TextStyle(color: Colors.black26)),
   textTheme: GoogleFonts.openSansTextTheme(),
   primaryColorDark: Colors.lightGreen,
   primaryColorLight: const Color(0xFF64DD17),
@@ -10,7 +14,7 @@ final theme = ThemeData(
   accentColor: const Color(0xFF009688),
   scaffoldBackgroundColor: const Color(0xFFE0F2F1),
   inputDecorationTheme: InputDecorationTheme(
-      fillColor: Colors.black54,
+      fillColor: Colors.red,
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
       ),
