4fdd0b214d10927ebf0f73d74978eeab83bd9c77
GanneyADortch <ganneyd@gmail.com>
2021-05-23 12:33:03 -0400

    login page

--- lib/features/grow/presentation/pages/login/widgets/form_widget/form_widget_1.dart
+++ lib/features/grow/presentation/pages/login/widgets/form_widget/form_widget_1.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:grow_run_v1/features/grow/presentation/pages/login/widgets/form_group/login_form_group.dart';
+import 'package:grow_run_v1/features/grow/presentation/widgets/default_ui_elements.dart';
 import 'package:reactive_forms/reactive_forms.dart';
+import '../../../../child/pages/sign_up/view/sign_up_page.dart';
 
 ///
 class LoginForm extends StatelessWidget {
@@ -17,19 +20,37 @@ class LoginForm extends StatelessWidget {
         builder: (context, form, child) {
           return Column(
             children: <Widget>[
-              ReactiveTextField<String>(
-                formControlName: 'email',
-              ),
+              DefaultUIElements.getEmailInput(
+                  formControlName: 'email', hintText: 'Email or Username'),
+              DefaultUIElements.getDefaultFormPadding(),
               ReactiveTextField<String>(
                 formControlName: 'password',
+                decoration: const InputDecoration(hintText: 'Password'),
               ),
               ElevatedButton(
                 onPressed: () => form.valid
                     ? handler(form.control('email').value.toString(),
                         form.control('password').value.toString())
                     : null,
-                child: const Text('submit'),
-              )
+                child: const Text('SUBMIT'),
+              ),
+              RichText(
+                text: TextSpan(
+                    text: "Don't have an account? ",
+                    style: Theme.of(context).textTheme.bodyText2,
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Sign up Here.',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontWeight: FontWeight.bold),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(context, ChildSignUpForm.route());
+                            })
+                    ]),
+              ),
             ],
           );
         });
