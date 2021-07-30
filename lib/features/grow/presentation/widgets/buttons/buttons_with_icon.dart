a7d3117c0ae017efec645f50fc21151671cac95f
mac_m1 <macminidevgd@gmail.com>
2021-07-29 23:57:15 -0400

    define widget that makes a button with icons

---
+++ lib/features/grow/presentation/widgets/buttons/buttons_with_icon.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+///Provides  buttons that have an icon withi
+class ButtonWithIcon extends StatelessWidget {
+  ///Provides a button with a default [icon size = 30]
+  ///that has the icon provided within
+  ///pass the [icon] [iconsize] if you want it to be bigger
+  ///[onPressed] for when the button is pressed
+  ///[button name]
+  //TODO sized box might overflow fix
+  ButtonWithIcon({
+    required IconData icon,
+    double width = 80,
+    double height = 80,
+    double scaleFactor = 0.55,
+    required VoidCallback onPressed,
+    required String buttonName,
+  })  : _icon = icon,
+        _width = width,
+        _height = height,
+        _onPressed = onPressed,
+        _buttonName = buttonName,
+        _scaleFactor = scaleFactor,
+        super(key: Key('$buttonName-button'));
+
+  final IconData _icon;
+  final double _width;
+  final double _height;
+  final VoidCallback _onPressed;
+  final String _buttonName;
+  final double _scaleFactor;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        shape: const RoundedRectangleBorder().copyWith(
+            borderRadius: const BorderRadius.all(Radius.circular(15))),
+        fillColor: Theme.of(context).buttonColor,
+        splashColor: Theme.of(context).splashColor,
+        onPressed: _onPressed,
+        child: SizedBox(
+          height: _height,
+          width: _width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FaIcon(
+                    _icon,
+                    size: _height < _width
+                        ? _height * _scaleFactor
+                        : _width * _scaleFactor,
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: RichText(
+                  text: TextSpan(
+                      text: _buttonName.toUpperCase(),
+                      style: Theme.of(context).primaryTextTheme.button),
+                ),
+              ))
+            ],
+          ),
+        ));
+  }
+}
