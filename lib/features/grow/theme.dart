a751d9261e24b6c90e97f05b710308d254a93da3
mac_m1 <macminidevgd@gmail.com>
2021-07-29 22:52:15 -0400

    added more color themes for icons app bar

--- lib/features/grow/theme.dart
+++ lib/features/grow/theme.dart
@@ -2,22 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 //TODO change to match GROW's app theme from old app
+///32
 final ThemeData theme = ThemeData(
   primaryTextTheme: TextTheme(
-      headline2: const TextStyle(color: Colors.black54),
-      bodyText1: TextStyle(color: Colors.grey.shade700),
-      bodyText2: const TextStyle(color: Colors.black26)),
+    headline2: const TextStyle(color: Colors.black54),
+    bodyText1: TextStyle(color: Colors.grey.shade700),
+    bodyText2: const TextStyle(color: Colors.black26),
+    button: const TextStyle(
+      color: Color.fromRGBO(2, 108, 73, 1),
+      fontFamily: 'Arial Rounded MT Bold',
+    ),
+  ),
+  //accentTextTheme: TextTheme(button: ),
+  primaryIconTheme: const IconThemeData(color: Colors.black),
+  accentIconTheme: const IconThemeData(color: Colors.black),
+  iconTheme: const IconThemeData(color: Color.fromRGBO(2, 108, 73, 1)),
+  appBarTheme: const AppBarTheme(color: Color.fromRGBO(144, 223, 43, 1)),
+  snackBarTheme: const SnackBarThemeData(
+      actionTextColor: Colors.pink,
+      contentTextStyle: TextStyle(color: Colors.white)),
   textTheme: GoogleFonts.openSansTextTheme(),
   primaryColorDark: Colors.lightGreen,
   primaryColorLight: const Color(0xFF64DD17),
   primaryColor: Colors.lightGreen,
-  accentColor: const Color(0xFF009688),
-  scaffoldBackgroundColor: const Color(0xFFE0F2F1),
+  accentColor: const Color.fromRGBO(2, 108, 73, 1),
+  scaffoldBackgroundColor: Colors.white, //const Color(0xFFE0F2F1),
   inputDecorationTheme: InputDecorationTheme(
       fillColor: Colors.red,
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
       ),
       focusColor: Colors.lightGreen),
-  buttonColor: Colors.lightGreen,
+  buttonColor: const Color.fromRGBO(144, 223, 43, 1),
+  splashColor: Colors.amber,
 );
