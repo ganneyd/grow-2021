3d2cec4dd98cf10ca9e135dc7994492360b4d57e
mac_m1 <macminidevgd@gmail.com>
2021-08-04 09:57:52 -0400

    updated theme

--- lib/features/grow/theme.dart
+++ lib/features/grow/theme.dart
@@ -4,26 +4,32 @@ import 'package:google_fonts/google_fonts.dart';
 //TODO change to match GROW's app theme from old app
 ///32
 final ThemeData theme = ThemeData(
+  // fontFamily: 'Arial Rounded MT Bold',
   primaryTextTheme: TextTheme(
+    headline1: const TextStyle(
+        color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
     headline2: const TextStyle(color: Colors.black54),
+    headline5: const TextStyle(
+        color: Colors.black26, fontWeight: FontWeight.w900, fontSize: 32),
     bodyText1: TextStyle(color: Colors.grey.shade700),
     bodyText2: const TextStyle(color: Colors.black26),
     button: const TextStyle(
       color: Color.fromRGBO(2, 108, 73, 1),
-      fontFamily: 'Arial Rounded MT Bold',
     ),
   ),
+
   //accentTextTheme: TextTheme(button: ),
   primaryIconTheme: const IconThemeData(color: Colors.black),
   accentIconTheme: const IconThemeData(color: Colors.black),
-  iconTheme: const IconThemeData(color: Color.fromRGBO(2, 108, 73, 1)),
+  iconTheme: const IconThemeData(
+    color: Color.fromRGBO(2, 108, 73, 1),
+  ),
   appBarTheme: const AppBarTheme(
     color: Color.fromRGBO(144, 223, 43, 1),
     textTheme: TextTheme(
       headline6: TextStyle(
         fontSize: 36,
         color: Colors.white,
-        fontFamily: 'Arial Rounded MT Bold',
         fontWeight: FontWeight.w500,
         letterSpacing: 2.0,
       ),
@@ -32,7 +38,7 @@ final ThemeData theme = ThemeData(
   snackBarTheme: const SnackBarThemeData(
       actionTextColor: Colors.pink,
       contentTextStyle: TextStyle(color: Colors.white)),
-  textTheme: GoogleFonts.openSansTextTheme(),
+  textTheme: Typography.blackCupertino,
   primaryColorDark: Colors.lightGreen,
   primaryColorLight: const Color(0xFF64DD17),
   primaryColor: Colors.lightGreen,
@@ -45,5 +51,5 @@ final ThemeData theme = ThemeData(
       ),
       focusColor: Colors.lightGreen),
   buttonColor: const Color.fromRGBO(144, 223, 43, 1),
-  splashColor: Colors.amber,
+  splashColor: const Color.fromRGBO(255, 204, 0, 1),
 );
