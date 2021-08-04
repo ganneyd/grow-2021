7a9d71345023793ef17ff185cb34f4a8c06cffca
mac_m1 <macminidevgd@gmail.com>
2021-08-04 10:11:47 -0400

    exported all the child views

--- lib/features/grow/presentation/child/views_bucket.dart
+++ lib/features/grow/presentation/child/views_bucket.dart
@@ -1,2 +1,8 @@
+export 'pages/goals/view/goals_page.dart';
 export 'pages/home/view/home_page.dart';
+export 'pages/leaderboard/view/leaderboard_page.dart';
+export 'pages/profile/view/profile_page.dart';
+export 'pages/routes/view/routes_page.dart';
+export 'pages/shop/view/shop_page.dart';
 export 'pages/sign_up/view/sign_up_page.dart';
+export 'pages/statistics/view/statistics_page.dart';
