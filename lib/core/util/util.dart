744de7ca8ee363b3f17d7d65f7a89fc43b1857c9
mac_m1 <macminidevgd@gmail.com>
2021-08-04 09:57:36 -0400

    fixed naming errors

--- lib/core/util/util.dart
+++ lib/core/util/util.dart
@@ -1,7 +1,7 @@
 export 'clothing_util.dart';
 export 'gender.dart';
-export 'privallge_util.dart';
-export 'school_util.dart';
 export 'goal_util.dart';
+export 'privilege_util.dart';
+export 'school_util.dart';
 export 'trophy_util.dart';
 export 'user_type_util.dart';
