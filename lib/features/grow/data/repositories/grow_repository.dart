d69c0998c420b5e7f155049b876a905c28cb5110
mac_m1 <macminidevgd@gmail.com>
2021-08-04 10:03:33 -0400

    moved _getSchools to school repo
    addded _getBuildInfo

--- lib/features/grow/data/repositories/grow_repository.dart
+++ lib/features/grow/data/repositories/grow_repository.dart
@@ -2,8 +2,10 @@ import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:grow_run_v1/core/error/exceptions.dart';
 import 'package:grow_run_v1/core/error/failures.dart';
+import 'package:grow_run_v1/core/util/build_info.dart';
 import 'package:grow_run_v1/features/grow/data/models/school/school_model.dart';
 import 'package:grow_run_v1/features/grow/domain/entities/school/school_entity.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import '../../domain/repositories/grow_repository.dart';
 import '../datasources/datasources.dart';
 
@@ -16,7 +18,7 @@ enum Collections {
 
 ///Implementation of the grow repository
 class GROWRepositoryImplementation extends GROWRepository {
-  ///Constrcutor , takes a [FireBaseAuth] instance
+  ///Constructor , takes a [FireBaseAuth] instance
   GROWRepositoryImplementation(RemoteDataSource remoteDataSource)
       : _firebaseAuth = FirebaseAuth.instance,
         _remoteDataSource = remoteDataSource;
@@ -24,27 +26,18 @@ class GROWRepositoryImplementation extends GROWRepository {
   final RemoteDataSource _remoteDataSource;
 
   @override
-  Future<Either<Failure, List<SchoolEntity>>> getSchools() async {
-    final List<SchoolModel> schools = <SchoolModel>[];
+  Future<Either<Failure, BuildInfo>> getBuildInfo() async {
     try {
-      //Attempt to retrieve the collection in JSON formatt
-      final List<Map<String, dynamic>> result =
-          await _remoteDataSource.getCollection('school');
-      //If no exceptions are thrown then proceed to convert the JSON data
-      //to school models, adding them to the [schools] list
-      for (final Map<String, dynamic> schoolJSON in result) {
-        schools.add(SchoolModel.fromJson(schoolJSON));
-      }
+      final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      final BuildInfo buildInfo = BuildInfo(
+          appName: packageInfo.appName,
+          version: packageInfo.version,
+          buildNumber: packageInfo.buildNumber);
 
-      //if successful then this function will return the list of schools
-      return Right<Failure, List<SchoolEntity>>(schools);
-    } on ReadDataException {
-      return Left<Failure, List<SchoolEntity>>(FetchDataFailure());
+      return Right<Failure, BuildInfo>(buildInfo);
     } catch (error) {
-      //this means we had an unexpected exception
-      //todo track this unexpected exception and log it to crashlytics
-      print('Error in grow  repod $error');
-      return Left<Failure, List<SchoolEntity>>(FetchDataFailure());
+      ///TODO crashlytics implementation
+      return const Left<Failure, BuildInfo>(FetchDataFailure());
     }
   }
 }
