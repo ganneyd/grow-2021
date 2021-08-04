779446869817fd4d637d26b94c0568039db3ac5f
mac_m1 <macminidevgd@gmail.com>
2021-08-04 10:07:24 -0400

    defined getBuildinfo

--- lib/features/grow/domain/repositories/grow_repository.dart
+++ lib/features/grow/domain/repositories/grow_repository.dart
@@ -1,16 +1,12 @@
 //this is the authentication  repository's contract
 import 'package:dartz/dartz.dart';
 import 'package:grow_run_v1/core/error/failures.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:grow_run_v1/core/util/util.dart';
-import 'package:grow_run_v1/features/grow/data/models/models_bucket.dart';
-import '../entities/user/user_entity.dart';
+import 'package:grow_run_v1/core/util/build_info.dart';
 import 'package:grow_run_v1/features/grow/domain/entities/entities_bucket.dart';
 
 ///The repository that handles all the business logic calls to apis and external
 ///services such as geolocation, animation stuff etc
 abstract class GROWRepository {
-  ///Gets the SchoolEntites from the datasource and return them or return a
-  ///failure as to why it failed
-  Future<Either<Failure, List<SchoolEntity>>> getSchools();
+  ///Retrieves the build number version and platform info
+  Future<Either<Failure, BuildInfo>> getBuildInfo();
 }
