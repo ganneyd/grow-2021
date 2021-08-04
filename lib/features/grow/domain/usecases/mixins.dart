71f3c1751209970700364f58d1b0994c520ae499
mac_m1 <macminidevgd@gmail.com>
2021-08-04 10:09:13 -0400

    removed to child with

--- lib/features/grow/domain/usecases/mixins.dart
+++ lib/features/grow/domain/usecases/mixins.dart
@@ -1,19 +1,6 @@
-import 'package:grow_run_v1/features/grow/domain/entities/child/child_entity.dart';
 import 'package:grow_run_v1/features/grow/domain/entities/parent/parent_entity.dart';
 
-ChildEntity toChilEntityWithID(ChildEntity child, String id, String parentID) {
-  return ChildEntity(
-      uid: id,
-      username: child.username,
-      firstname: child.firstname,
-      lastname: child.lastname,
-      gender: child.gender,
-      dateOfBirth: child.dateOfBirth,
-      gradeLevel: child.gradeLevel,
-      parentID: parentID,
-      schoolID: child.schoolID);
-}
-
+///Returns a parent entity with id
 ParentEntity toParentEntityWithID(ParentEntity parent, String id) {
   return ParentEntity(
       uid: id,
