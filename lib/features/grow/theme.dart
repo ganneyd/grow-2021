8a50d833c34d793231a20b3fb5c96852fc83dac9
mac_m1 <macminidevgd@gmail.com>
2021-08-06 21:45:39 -0400

    trying to find what text theme belongs to what

--- lib/features/grow/theme.dart
+++ lib/features/grow/theme.dart
@@ -5,38 +5,53 @@ import 'package:google_fonts/google_fonts.dart';
 ///32
 final ThemeData theme = ThemeData(
     // fontFamily: 'Arial Rounded MT Bold',
+
     primaryTextTheme: TextTheme(
-      headline1: const TextStyle(
+      headline1: GoogleFonts.balooThambi().copyWith(
           color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-      headline2: const TextStyle(color: Colors.black54),
-      headline4: const TextStyle(
+      headline2: GoogleFonts.balooThambi().copyWith(color: Colors.black54),
+      subtitle1: GoogleFonts.balooThambi().copyWith(fontSize: 40),
+      subtitle2: GoogleFonts.balooThambi().copyWith(fontSize: 40),
+      headline4: GoogleFonts.balooThambi().copyWith(
           color: Colors.white, fontSize: 35, fontWeight: FontWeight.w800),
-      headline5: const TextStyle(
+      headline5: GoogleFonts.balooThambi().copyWith(
           color: Colors.black26, fontWeight: FontWeight.w900, fontSize: 32),
-      headline6: const TextStyle(
+      headline6: GoogleFonts.balooThambi().copyWith(
         color: Colors.white,
         fontSize: 20,
         fontWeight: FontWeight.w600,
       ),
-      bodyText1: TextStyle(color: Colors.grey.shade700),
-      bodyText2: const TextStyle(color: Colors.black26),
-      button: const TextStyle(
+      bodyText1:
+          GoogleFonts.balooThambi().copyWith(color: Colors.grey.shade700),
+      bodyText2: GoogleFonts.balooThambi().copyWith(color: Colors.black26),
+      button: GoogleFonts.balooThambi().copyWith(
         color: Color.fromRGBO(2, 108, 73, 1),
       ),
     ),
-
+    fontFamily: GoogleFonts.balooThambi().fontFamily,
     //accentTextTheme: TextTheme(button: ),
     primaryIconTheme: const IconThemeData(color: Colors.black),
     accentIconTheme: const IconThemeData(color: Colors.black),
     iconTheme: const IconThemeData(
       color: Color.fromRGBO(2, 108, 73, 1),
     ),
-    appBarTheme: const AppBarTheme(
-      color: Color.fromRGBO(144, 223, 43, 1),
+    appBarTheme: AppBarTheme(
+      color: const Color.fromRGBO(144, 223, 43, 1),
+      titleTextStyle: TextStyle(
+          fontSize: 36,
+          fontFamily: GoogleFonts.balooThambi().fontFamily,
+          shadows: const <Shadow>[
+            Shadow(
+              offset: Offset(0, 4),
+              blurRadius: 6.0,
+              color: Color.fromRGBO(0, 0, 0, 0.10),
+            ),
+          ]),
       textTheme: TextTheme(
         headline6: TextStyle(
           fontSize: 36,
-          shadows: <Shadow>[
+          fontFamily: GoogleFonts.balooThambi().fontFamily,
+          shadows: const <Shadow>[
             Shadow(
               offset: Offset(0, 4),
               blurRadius: 6.0,
@@ -49,10 +64,16 @@ final ThemeData theme = ThemeData(
         ),
       ),
     ),
-    snackBarTheme: const SnackBarThemeData(
+    snackBarTheme: SnackBarThemeData(
         actionTextColor: Colors.pink,
-        contentTextStyle: TextStyle(color: Colors.white)),
-    textTheme: Typography.blackCupertino,
+        contentTextStyle: GoogleFonts.balooThambi().copyWith(
+          color: Colors.white,
+        )),
+    textTheme: TextTheme(
+      headline6: GoogleFonts.balooThambi(),
+      subtitle1: GoogleFonts.balooThambi(),
+      subtitle2: GoogleFonts.balooThambi(),
+    ),
     primaryColorDark: Colors.lightGreen,
     primaryColorLight: const Color(0xFF64DD17),
     primaryColor: const Color.fromRGBO(144, 223, 43, 1),
