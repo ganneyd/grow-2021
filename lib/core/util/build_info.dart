f5a0cb3d0806fb5e401be80f17b284bdb4966de5
mac_m1 <macminidevgd@gmail.com>
2021-08-06 21:45:13 -0400

    added flag for android or iOS

--- lib/core/util/build_info.dart
+++ lib/core/util/build_info.dart
@@ -2,12 +2,17 @@
 class BuildInfo {
   ///Takes [appName] [version] [buildNumber]
   const BuildInfo(
-      {required this.appName,
-      required this.version,
-      required this.buildNumber});
+      {String? appName, String? version, String? buildNumber, bool? isAndroid})
+      : appName = appName ?? 'GROW',
+        buildNumber = buildNumber ?? '0',
+        version = version ?? 'err',
+        isAndroid = isAndroid ?? true;
 
   ///[appName] name of the app
   ///[version] current version of the app
   ///[buildNumber] current build number for the app
   final String appName, version, buildNumber;
+
+  ///Whether or not the os is android or iOS
+  final bool isAndroid;
 }
