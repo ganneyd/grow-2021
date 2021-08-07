59c0378ecfbe0f2f6a147de7920f7ca1e137bf10
mac_m1 <macminidevgd@gmail.com>
2021-08-07 01:20:10 -0400

    displays a page with the user''s medals

---
+++ lib/features/grow/presentation/child/pages/store/sub_views/medals/views/medals_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+///
+class Medals extends StatelessWidget {
+  ///
+  const Medals() : super(key: const Key('child-store-medals-page'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
