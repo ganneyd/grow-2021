f73e38794b16c6db36fc924a5e1442e18b7e11da
mac_m1 <macminidevgd@gmail.com>
2021-08-17 23:19:38 -0400

    added loggin to project

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:logging/logging.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord record) {
+    print('${record.level.name}: ${record.loggerName}: ${record.message}');
+  });
   // final String host =
   //     Platform.isAndroid ? '192.168.1.2:8080' : 'http://localhost:8080';
-  await FirebaseAuth.instance.useAuthEmulator('192.168.1.2', 9099);
-  FirebaseFunctions.instance.useFunctionsEmulator('192.168.1.2', 5001);
+  await FirebaseAuth.instance.useAuthEmulator('192.168.1.9', 9099);
+  FirebaseFunctions.instance.useFunctionsEmulator('192.168.1.9', 5001);
 
   FirebaseFirestore.instance.settings = const Settings(
-    host: '192.168.1.2:8080',
+    host: '192.168.1.9:8080',
     sslEnabled: false,
   );
 
