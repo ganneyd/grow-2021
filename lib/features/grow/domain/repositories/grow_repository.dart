a1ff14720183d66cc5d2140818a3ee3a36a28dff
mac_m1 <macminidevgd@gmail.com>
2021-08-17 23:24:00 -0400

    added stopwatch stream

--- lib/features/grow/domain/repositories/grow_repository.dart
+++ lib/features/grow/domain/repositories/grow_repository.dart
@@ -2,6 +2,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:grow_run_v1/core/error/failures.dart';
 import 'package:grow_run_v1/core/util/build_info.dart';
+import 'package:grow_run_v1/features/grow/data/models/stop_watch/stop_watch_model.dart';
 import 'package:grow_run_v1/features/grow/domain/entities/entities_bucket.dart';
 
 ///The repository that handles all the business logic calls to apis and external
@@ -9,4 +10,7 @@ import 'package:grow_run_v1/features/grow/domain/entities/entities_bucket.dart';
 abstract class GROWRepository {
   ///Retrieves the build number version and platform info
   Future<Either<Failure, BuildInfo>> getBuildInfo();
+
+  ///exposes stream that streams times
+  Stream<ElapsedTimeModel> stopWatchStream();
 }
