a3042d840db3f0b2c98b0331313b75ba9c69e347
mac_m1 <macminidevgd@gmail.com>
2021-08-21 21:24:50 -0400

    entity to represent the previous values

---
+++ lib/features/grow/domain/entities/previous/previous_entity.dart
@@ -0,0 +1,17 @@
+///The previous values of objects
+class Previous {
+  ///
+  Previous(
+      {required this.distance,
+      required this.latitude,
+      required this.longitude});
+
+  ///the previous longitude
+  final double longitude;
+
+  /// the previous longitude
+  final double latitude;
+
+  ///the previous latitude
+  final double distance;
+}
