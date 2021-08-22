863ea960917aee83af0cedd03e2ea94bce7c9fcd
mac_m1 <macminidevgd@gmail.com>
2021-08-21 20:27:44 -0400

    model to represent previous chnages

---
+++ lib/features/grow/data/models/previous/previous_model.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:grow_run_v1/features/grow/domain/entities/previous/previous_entity.dart';
+
+part 'previous_model.freezed.dart';
+
+///
+@freezed
+class PreviousModel extends Previous with _$PreviousModel {
+  ///
+  const factory PreviousModel({
+    @Default(0) double longitude,
+    @Default(0) double latitude,
+    @Default(0) double distance,
+  }) = _PreviousModel;
+}
---
+++ lib/features/grow/data/models/previous/previous_model.freezed.dart
@@ -0,0 +1,190 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'previous_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$PreviousModelTearOff {
+  const _$PreviousModelTearOff();
+
+  _PreviousModel call(
+      {double longitude = 0, double latitude = 0, double distance = 0}) {
+    return _PreviousModel(
+      longitude: longitude,
+      latitude: latitude,
+      distance: distance,
+    );
+  }
+}
+
+/// @nodoc
+const $PreviousModel = _$PreviousModelTearOff();
+
+/// @nodoc
+mixin _$PreviousModel {
+  double get longitude => throw _privateConstructorUsedError;
+  double get latitude => throw _privateConstructorUsedError;
+  double get distance => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $PreviousModelCopyWith<PreviousModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PreviousModelCopyWith<$Res> {
+  factory $PreviousModelCopyWith(
+          PreviousModel value, $Res Function(PreviousModel) then) =
+      _$PreviousModelCopyWithImpl<$Res>;
+  $Res call({double longitude, double latitude, double distance});
+}
+
+/// @nodoc
+class _$PreviousModelCopyWithImpl<$Res>
+    implements $PreviousModelCopyWith<$Res> {
+  _$PreviousModelCopyWithImpl(this._value, this._then);
+
+  final PreviousModel _value;
+  // ignore: unused_field
+  final $Res Function(PreviousModel) _then;
+
+  @override
+  $Res call({
+    Object? longitude = freezed,
+    Object? latitude = freezed,
+    Object? distance = freezed,
+  }) {
+    return _then(_value.copyWith(
+      longitude: longitude == freezed
+          ? _value.longitude
+          : longitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      latitude: latitude == freezed
+          ? _value.latitude
+          : latitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      distance: distance == freezed
+          ? _value.distance
+          : distance // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$PreviousModelCopyWith<$Res>
+    implements $PreviousModelCopyWith<$Res> {
+  factory _$PreviousModelCopyWith(
+          _PreviousModel value, $Res Function(_PreviousModel) then) =
+      __$PreviousModelCopyWithImpl<$Res>;
+  @override
+  $Res call({double longitude, double latitude, double distance});
+}
+
+/// @nodoc
+class __$PreviousModelCopyWithImpl<$Res>
+    extends _$PreviousModelCopyWithImpl<$Res>
+    implements _$PreviousModelCopyWith<$Res> {
+  __$PreviousModelCopyWithImpl(
+      _PreviousModel _value, $Res Function(_PreviousModel) _then)
+      : super(_value, (v) => _then(v as _PreviousModel));
+
+  @override
+  _PreviousModel get _value => super._value as _PreviousModel;
+
+  @override
+  $Res call({
+    Object? longitude = freezed,
+    Object? latitude = freezed,
+    Object? distance = freezed,
+  }) {
+    return _then(_PreviousModel(
+      longitude: longitude == freezed
+          ? _value.longitude
+          : longitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      latitude: latitude == freezed
+          ? _value.latitude
+          : latitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      distance: distance == freezed
+          ? _value.distance
+          : distance // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_PreviousModel implements _PreviousModel {
+  const _$_PreviousModel(
+      {this.longitude = 0, this.latitude = 0, this.distance = 0});
+
+  @JsonKey(defaultValue: 0)
+  @override
+  final double longitude;
+  @JsonKey(defaultValue: 0)
+  @override
+  final double latitude;
+  @JsonKey(defaultValue: 0)
+  @override
+  final double distance;
+
+  @override
+  String toString() {
+    return 'PreviousModel(longitude: $longitude, latitude: $latitude, distance: $distance)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _PreviousModel &&
+            (identical(other.longitude, longitude) ||
+                const DeepCollectionEquality()
+                    .equals(other.longitude, longitude)) &&
+            (identical(other.latitude, latitude) ||
+                const DeepCollectionEquality()
+                    .equals(other.latitude, latitude)) &&
+            (identical(other.distance, distance) ||
+                const DeepCollectionEquality()
+                    .equals(other.distance, distance)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(longitude) ^
+      const DeepCollectionEquality().hash(latitude) ^
+      const DeepCollectionEquality().hash(distance);
+
+  @JsonKey(ignore: true)
+  @override
+  _$PreviousModelCopyWith<_PreviousModel> get copyWith =>
+      __$PreviousModelCopyWithImpl<_PreviousModel>(this, _$identity);
+}
+
+abstract class _PreviousModel implements PreviousModel {
+  const factory _PreviousModel(
+      {double longitude, double latitude, double distance}) = _$_PreviousModel;
+
+  @override
+  double get longitude => throw _privateConstructorUsedError;
+  @override
+  double get latitude => throw _privateConstructorUsedError;
+  @override
+  double get distance => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$PreviousModelCopyWith<_PreviousModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
