626852ea87781a56024994253abc520aae2fd998
mac_m1 <macminidevgd@gmail.com>
2021-09-01 19:43:41 -0400

    added stats cubit

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,12 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:grow_run_v1/features/grow/data/datasources/datasources.dart';
+import 'package:grow_run_v1/features/grow/data/repositories/authentication_repository.dart';
 import 'package:grow_run_v1/features/grow/data/repositories/grow_repository.dart';
 import 'package:grow_run_v1/features/grow/data/repositories/location_repositor_implementation.dart';
+import 'package:grow_run_v1/features/grow/domain/repositories/authentication_repository.dart';
 import 'package:grow_run_v1/features/grow/domain/repositories/grow_repository.dart';
 import 'package:grow_run_v1/features/grow/domain/repositories/location_repository.dart';
 import 'package:grow_run_v1/features/grow/domain/repositories/run_details_repository.dart';
 import 'package:grow_run_v1/features/grow/presentation/child/pages/run/widgets/sub_cubit/timer_cubit.dart';
+import 'package:grow_run_v1/features/grow/presentation/child/pages/statistics/cubit/stats_cubit.dart';
 
 import 'features/grow/data/repositories/run_repository_implementation.dart';
 
@@ -19,19 +23,27 @@ Future<void> init() async {
 
 //!Child
 //BLOCs
+//Stats page
+  serviceLocator.registerFactory<StatsCubit>(
+      () => StatsCubit(runDetailsRepository: serviceLocator()));
 //RunPage
   serviceLocator.registerFactory<TimerCubit>(() => TimerCubit(
+      authenticationRepository: serviceLocator(),
       runDetailsRepository: serviceLocator(),
       locationRepository: serviceLocator(),
       growRepository: serviceLocator()));
 //!UseCases
 //!Repositories
+  serviceLocator.registerLazySingleton<AuthenticationRepository>(
+      () => AuthenticationRepositoryImplementation(FirebaseAuth.instance));
   serviceLocator.registerLazySingleton<LocationRepository>(
       () => LocationRepositoryImplementation());
   serviceLocator.registerLazySingleton<GROWRepository>(
       () => GROWRepositoryImplementation(serviceLocator()));
   serviceLocator.registerLazySingleton<RunDetailsRepository>(
       () => RunRepositoryImplementation(remoteDataSource: serviceLocator()));
+//!Services
+
 //!DataSources
   serviceLocator.registerLazySingleton<RemoteDataSource>(
       () => RemoteDataSourceImplementation(FirebaseFirestore.instance));
