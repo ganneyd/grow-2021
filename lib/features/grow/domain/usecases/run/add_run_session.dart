d11499d76e6b9ace0b7682955912cfc031aec7b3
mac_m1 <macminidevgd@gmail.com>
2021-09-01 20:26:51 -0400

    implemented usecase to add run

--- lib/features/grow/domain/usecases/run/add_run_session.dart
+++ lib/features/grow/domain/usecases/run/add_run_session.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 import 'package:grow_run_v1/core/error/failures.dart';
 import 'package:grow_run_v1/core/usecases/usecases.dart';
 import 'package:grow_run_v1/features/grow/domain/entities/run_details_entity.dart';
+import 'package:grow_run_v1/features/grow/domain/repositories/authentication_repository.dart';
 import 'package:grow_run_v1/features/grow/domain/repositories/run_details_repository.dart';
 
 ///When the user has finished a run it is added to their runs
@@ -10,13 +11,22 @@ class AddRun implements UseCase<void, Params> {
   ///Takes an instance of [RunDetailsRepository]
   const AddRun({
     required RunDetailsRepository runDetailsRepository,
-  }) : _runDetailsRepository = runDetailsRepository;
+    required AuthenticationRepository authenticationRepository,
+  })  : _runDetailsRepository = runDetailsRepository,
+        _authenticationRepository = authenticationRepository;
 
   final RunDetailsRepository _runDetailsRepository;
+  final AuthenticationRepository _authenticationRepository;
   @override
   Future<Either<Failure, void>> call(Params params) {
-    return _runDetailsRepository.addRunSession(
-        runSession: params.runSession, sessionDate: DateTime.now());
+    final Either<Failure, String> results =
+        _authenticationRepository.getCurrentUserID();
+    return results.fold(
+        (Failure l) => Future.value(Left<Failure, void>(l)),
+        (String uid) => _runDetailsRepository.addRunSession(
+            uid: uid,
+            runSession: params.runSession,
+            sessionDate: DateTime.now()));
   }
 }
 
