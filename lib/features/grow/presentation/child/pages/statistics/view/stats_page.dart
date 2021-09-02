cddd8ac2054ae74eabb5c5c1a0403e44f806fa8c
mac_m1 <macminidevgd@gmail.com>
2021-09-01 20:21:38 -0400

    page to display statistics

--- lib/features/grow/presentation/child/pages/statistics/view/stats_page.dart
+++ lib/features/grow/presentation/child/pages/statistics/view/stats_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grow_run_v1/features/grow/presentation/child/pages/widgets/chart.dart';
 import 'package:grow_run_v1/features/grow/presentation/child/pages/widgets/sliver_app_bar_with_shape.dart';
 import 'package:grow_run_v1/features/grow/presentation/child/pages/statistics/cubit/stats_cubit.dart';
 import 'package:grow_run_v1/features/grow/presentation/child/pages/statistics/cubit/stats_state.dart';
+import 'package:grow_run_v1/features/grow/presentation/pages/splash_page.dart';
+
+import '../../../../../../../service_locator.dart';
 
 class StatsPage extends StatelessWidget {
   const StatsPage({Key? key}) : super(key: key);
@@ -11,27 +15,39 @@ class StatsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final ScrollController _myScrollController = ScrollController();
     return BlocProvider<StatsCubit>(
-      create: (_) => StatsCubit(),
+      create: (_) => serviceLocator<StatsCubit>()..init(),
       child: BlocConsumer<StatsCubit, StatsState>(
         listener: (BuildContext context, StatsState state) {},
         builder: (BuildContext context, StatsState state) {
-          return Theme(
-              data: ThemeData(accentColor: Theme.of(context).primaryColor),
-              child: Scaffold(
-                body: CustomScrollView(
-                  controller: _myScrollController,
-                  slivers: <Widget>[
-                    SliverAppBarWithShape(
-                        pageName: 'Stats',
-                        scrollController: _myScrollController,
-                        titleName: 'Goals',
-                        backgroundColor: Theme.of(context).primaryColor,
-                        background: Align(
-                          child: Text('Hey'),
-                        ))
-                  ],
-                ),
-              ));
+          if (state.status.isLoading()) {
+            return SplashPage();
+          } else if (state.status.loadedUnsuccessfully()) {
+          } else if (state.status.loadedSuccessfully()) {
+            return Theme(
+                data: ThemeData(accentColor: Theme.of(context).primaryColor),
+                child: Scaffold(
+                  body: CustomScrollView(
+                    controller: _myScrollController,
+                    slivers: <Widget>[
+                      SliverAppBarWithShape(
+                          pageName: 'Stats',
+                          scrollController: _myScrollController,
+                          titleName: 'Goals',
+                          backgroundColor: Theme.of(context).primaryColor,
+                          background: Align(
+                            child: Text('Hey'),
+                          )),
+                      SliverList(delegate: SliverChildBuilderDelegate(
+                          (BuildContext context, int index) {
+                        Chart<DistanceChartData>(
+                          dataList: state.distanceStatsList,
+                        );
+                      }))
+                    ],
+                  ),
+                ));
+          }
+          return Text(state.status.toString());
         },
       ),
     );
